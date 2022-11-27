b903305786ab76994c48f3063eaa81e10d39058a
Wael Karman <wael.karman@gmail.com>
2022-11-27 13:28:21 +0100

    Update BeautifulTriplets.py

--- BeautifulTriplets.py
+++ BeautifulTriplets.py
@@ -4,6 +4,8 @@ def beautifulTriplets(d, arr):
     count = 0
     for i in range(len(arr)):
         for j in range(i+1,len(arr)):
+            if((arr[j]-arr[i])!=d):
+                continue
             for k in range(j+1,len(arr)):
                 if( ((arr[j]-arr[i]) == (arr[k]-arr[j])) and ((arr[k]-arr[j]) == d) ):
                     count += 1
