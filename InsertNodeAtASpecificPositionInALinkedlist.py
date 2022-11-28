31d7c57d053638e21e8cd8385d15020cf54f7551
Wael Karman <wael.karman@gmail.com>
2022-11-28 16:51:41 +0100

    Update InsertNodeAtASpecificPositionInALinkedlist.py

--- InsertNodeAtASpecificPositionInALinkedlist.py
+++ InsertNodeAtASpecificPositionInALinkedlist.py
@@ -20,4 +20,23 @@ def insertNodeAtPosition(llist, data, position):
         
     return head
 
-
+def insertNodeAtPosition(llist, data, position):
+    # Write your code here
+    if(position == 0):
+        head = SinglyLinkedListNode(data)
+        head.next = llist
+        return head
+    else:
+        head = llist
+        old_llist = llist
+        count = 0
+        while(llist is not None):
+            if(position == count):
+                new_node = SinglyLinkedListNode(data)
+                old_llist.next = new_node
+                new_node.next = llist
+                break
+            old_llist = llist
+            llist = llist.next
+            count += 1
+        return head
