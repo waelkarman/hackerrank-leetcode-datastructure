e6f734c77e7a5d678fb72c2b41af4479376fc188
Wael Karman <wkarman@data-modul.com>
2023-10-04 13:17:19 +0200

    add solution

--- Graphs/DFS-BFS.py
+++ Graphs/DFS-BFS.py
@@ -1,17 +1,18 @@
+# DFS and BFS for undirected graphs
 
 g={
-    1:[2,3,4],
-    2:[1,3,5],
-    3:[1,4,6],
-    4:[1,3,6],
-    5:[2,3],
-    6:[3,4]
+    1:[2,3],
+    2:[1,3],
+    3:[1,2,4,7],
+    4:[3,5,6],
+    5:[4],
+    6:[4],
+    7:[3]
 }
 
-
-def dfs(g):
-    l=list(g.keys())
-    queue = [l[0]]
+# iterative
+def dfs(g,first):
+    queue = [first]
     done = []
     while len(queue)>0:
         position=queue.pop()
@@ -21,10 +22,17 @@ def dfs(g):
             if p not in queue and p not in done :
                 queue.append(p)
 
+# recursive
+def dfs_recursive(g,first,done):
+    print(f"{first}")
+    done.append(first)
+    for p in g[first]:
+        if p not in done:
+            dfs_recursive(g,p,done)
 
-def bfs(g):
-    l=list(g.keys())
-    stack = [l[0]]
+# iterative
+def bfs(g,first):
+    stack = [first]
     done = []
 
     while len(stack)>0:
@@ -35,7 +43,28 @@ def bfs(g):
             if(p not in stack and p not in done):
                 stack.append(p)
 
+# recursive
+def bfs_recursive(g,first,queue,done):
+    print(f"{first}")
+    done.append(first)
+    for p in g[first]:
+        if p not in queue and p not in done:
+            queue.append(p)
+    if len(queue)>0:
+        bfs_recursive(g,queue.pop(0),queue,done)
+
+
+
+l=list(g.keys())
 print("DFS:")
-dfs(g)
+dfs(g,l[0])
 print("BFS:")
-bfs(g)
+bfs(g,l[0])
+#RECURSIVE
+print("DFS_recursive:")
+queue=[]
+dfs_recursive(g,l[0],queue)
+print("BFS_recursive:")
+queue=[]
+done=[]
+bfs_recursive(g,l[0],queue,done)
