546b569acfd1dce9f8407781bab2a8a3a55d1713
Wael Karman <wkarman@data-modul.com>
2023-10-12 23:39:07 +0200

    improve exercise solution

--- RandomCodingChallenge/ClosestNumbers.cpp
+++ RandomCodingChallenge/ClosestNumbers.cpp
@@ -13,3 +13,21 @@ vector<int> closestNumbers(vector<int> arr) {
     std::vector<int> num_vect(num_map[min_key].begin(), num_map[min_key].end());
     return num_vect;
 }
+
+
+// UPDATED AND MORE EFFICIENT SOLUTION :
+
+
+vector<int> closestNumbers(vector<int> arr) {
+    map<int,vector<int>> num_map;
+    sort(arr.begin(),arr.end());
+    for(int i = 0; i<arr.size()-1;i++){
+        num_map[abs(arr[i]-arr[i+1])].push_back(arr[i]);
+        num_map[abs(arr[i]-arr[i+1])].push_back(arr[i+1]);
+    }
+    
+    map<int,vector<int>>::iterator it = num_map.begin();
+    int min_key = it->first;
+
+    return num_map[min_key];
+}
