040a9dddff50a23cd2c5d543ab2f36434990c0fd
Wael Karman <wkarman@data-modul.com>
2023-10-22 19:29:03 +0200

    add exercise solution

--- Backtracking-Momorization-Dynamic/CanSum/tabulation.py
+++ Backtracking-Momorization-Dynamic/CanSum/tabulation.py
@@ -1,1 +1,19 @@
 
+def cansumTab(target,array):
+
+    cs = [False for _ in range(target+1)]
+
+    # initialization
+    cs[0]=True
+    for a in array:
+        cs[a]=True
+
+    for i in range(target+1):
+        for k in range(i,target+1):
+            if cs[k] == True and i+k <= target:
+                cs[i+k] = True
+            
+    print(cs)
+    return cs[len(cs)-1]
+
+print(cansumTab(7,[3,4,5]))
